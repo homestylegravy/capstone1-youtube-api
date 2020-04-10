f0be58933fc025d91bed74d1b81f6048fc8bd2f2
homestylegravy <homestylegravy>
2020-04-10 06:11:52 -0500

    added rank and duration

--- scraper.py
+++ scraper.py
@@ -1,30 +1,17 @@
-import requests, sys, time, os, argparse
+import requests, sys, time, os, argparse, json, csv, pickle
 
 # List of simple to collect features
-snippet_features = ["title",
-                    "publishedAt",
-                    "channelId",
-                    "channelTitle",
-                    "categoryId"]
+snippet_features = ["title", "publishedAt", "channelId", "channelTitle", "categoryId"]
+
+# Used to identify columns, currently hardcoded order
+header = ["rank", "video_id"] + snippet_features + ["category", "trending_date", "tags", "view_count", "likes", "dislikes", "comment_count","duration"]
+                                            
+things_that_i_dont_want_to_include = ["thumbnail_link", "comments_disabled","ratings_disabled", "description"]
 
 # Any characters to exclude, generally these are things that become problematic in CSV files
 unsafe_characters = ['\n', '"']
 
-# Used to identify columns, currently hardcoded order
-header = ["video_id"] + snippet_features + ["trending_date", "tags", "view_count", "likes", "dislikes",
-                                            "comment_count", "thumbnail_link", "comments_disabled",
-                                            "ratings_disabled", "description"]
-
-
-def setup(api_path, code_path):
-    with open(api_path, 'r') as file:
-        api_key = file.readline()
-
-    with open(code_path) as file:
-        country_codes = [x.rstrip() for x in file]
-
-    return api_key, country_codes
-
+cats = {1: 'Film & Animation', 2: 'Autos & Vehicles', 10: 'Music', 15: 'Pets & Animals', 17: 'Sports', 18: 'Short Movies', 19: 'Travel & Events', 20: 'Gaming', 21: 'Videoblogging', 22: 'People & Blogs', 23: 'Comedy', 24: 'Entertainment', 25: 'News & Politics', 26: 'Howto & Style', 27: 'Education', 28: 'Science & Technology', 29: 'Nonprofits & Activism', 30: 'Movies', 31: 'Anime/Animation', 32: 'Action/Adventure', 33: 'Classics', 34: 'Comedy', 35: 'Documentary', 36: 'Drama', 37: 'Family', 38: 'Foreign', 39: 'Horror', 40: 'Sci-Fi/Fantasy', 41: 'Thriller', 42: 'Shorts', 43: 'Shows', 44: 'Trailers'}
 
 def prepare_feature(feature):
     # Removes any character from the unsafe characters list and surrounds the whole item in quotes
@@ -33,17 +20,36 @@ def prepare_feature(feature):
     return f'"{feature}"'
 
 
-def api_request(page_token, country_code="US", n=50):
-    # Builds the URL and requests the JSON from it
-    request_url = f"https://www.googleapis.com/youtube/v3/videos?part=id,statistics,snippet{page_token}chart=mostPopular&regionCode={country_code}&key={api_key}"    
-#     request_url = f"https://www.googleapis.com/youtube/v3/videos?part=id,statistics,snippet{page_token}chart=mostPopular&regionCode={country_code}&maxResults={n}&key={api_key}"
-    print("page_token:", page_token)
-    print("request_url:", request_url)
-    request = requests.get(request_url)
+def api_request(pageToken='', by_cat=False, trending=True, catId='', items=[], n=0):
+#     Calls the API and returns a list of items. It has to iterate through recursively
+#     a few times because the full response is split up into multiple pages
+    
+    url_base  = 'https://www.googleapis.com/youtube/v3/videos?'
+    features  =f'part=snippet%2CcontentDetails%2Cstatistics&{pageToken}'
+    category  =f'videoCategoryId={catId}&' if by_cat else ''
+    chart     = 'chart=mostPopular&' if trending else ''
+    other     =f'{chart}regionCode=US&{category}key='
+    url       =  url_base + features + other + api_key
+    response  =  requests.get(url).json()
+    
     if request.status_code == 429:
         print("Temp-Banned due to excess requests, please wait and continue later")
         sys.exit()
-    return request.json()
+    
+    itms_lst  =  response['items']
+    for item in itms_lst:
+        n+=1
+        item['rank'] = n
+        items.append(item)
+        
+#     The response doesn't return all the results. Use the token to get the next page
+    token    =  response.get("nextPageToken", None)
+    if token != None:
+        pageToken = f'pageToken={token}&'
+        api_request(pageToken=pageToken, items = items, n=n)
+    with open(f'items_{time.strftime('%y.%m.%d.%H:%M')}.pickle', 'wb') as pickly:
+        pickle.dump(items, pickly)    
+    return items
 
 
 def get_tags(tags_list):
@@ -51,18 +57,30 @@ def get_tags(tags_list):
     return prepare_feature("|".join(tags_list))
 
 
-def get_videos(items):
+def calc_seconds(duration):
+#     Takes the YouTube duration field and calculated seconds
+    pattern = '[0-9]+[HMS]'
+    result = re.findall(pattern, duration)
+    #result for 10 hours, 1 minute, 26 seconds = ['10H', '1M', '26S']
+    
+    nums = [int(val[:-1]) for val in result]
+    
+    if len(nums) == 1:
+        nums = [0, 0].append(nums[0])
+    elif len(nums) == 2:
+        nums = [0] + nums
+
+    seconds = nums[0] * 60 * 60 + nums[1]*60 + nums[2]
+    return seconds
+
+def parse_videos(items):
     lines = []
     for video in items:
-        comments_disabled = False
-        ratings_disabled = False
-
-        # We can assume something is wrong with the video if it has no statistics, often this means it has been deleted
-        # so we can just skip it
+        # We can assume something's wrong with a video with no stats, likely deleted
         if "statistics" not in video:
             continue
-
         # A full explanation of all of these features can be found on the GitHub page for this project
+        rank = video['rank']       #I added this rank during processing
         video_id = prepare_feature(video['id'])
 
         # Snippet and statistics are sub-dicts of video, containing the most useful info
@@ -71,86 +89,50 @@ def get_videos(items):
 
         # This list contains all of the features in snippet that are 1 deep and require no special processing
         features = [prepare_feature(snippet.get(feature, "")) for feature in snippet_features]
+#         Add the text of the category
+        features.append(prepare_feature(cats[features[-1]]))
 
         # The following are special case features which require unique processing, or are not within the snippet dict
         description = snippet.get("description", "")
-        thumbnail_link = snippet.get("thumbnails", dict()).get("default", dict()).get("url", "")
-        trending_date = time.strftime("%y.%d.%m")
+        trending_date = time.strftime("%y.%m.%d")
         tags = get_tags(snippet.get("tags", ["[none]"]))
         view_count = statistics.get("viewCount", 0)
-
-        # This may be unclear, essentially the way the API works is that if a video has comments or ratings disabled
-        # then it has no feature for it, thus if they don't exist in the statistics dict we know they are disabled
-        if 'likeCount' in statistics and 'dislikeCount' in statistics:
-            likes = statistics['likeCount']
-            dislikes = statistics['dislikeCount']
-        else:
-            ratings_disabled = True
-            likes = 0
-            dislikes = 0
-
-        if 'commentCount' in statistics:
-            comment_count = statistics['commentCount']
-        else:
-            comments_disabled = True
-            comment_count = 0
+        duration = calc_seconds(contentDetails.get("duration", 0))
+        likes = statistics.get('likeCount', 0)
+        dislikes = statistics.get('dislikeCount', 0)
+        comment_count = statistics.get('commentCount', 0)
 
         # Compiles all of the various bits of info into one consistently formatted line
-        line = [video_id] + features + [prepare_feature(x) for x in [trending_date, tags, view_count, likes, dislikes, comment_count, thumbnail_link, comments_disabled, ratings_disabled, description]]
+        line = [rank, video_id] + features + [prepare_feature(x) for x in [trending_date, tags], [view_count, duration, likes, dislikes, comment_count]]
         lines.append(",".join(line))
     return lines
 
 
-def get_pages(country_code, next_page_token="&"):
-    country_data = []
+def write_to_file(lines):
+    print(f"Writing video data to file {time.strftime('%y.%m.%d.%H:%M')}...")
 
-    # Because the API uses page tokens (which are literally just the same function 
-    # of numbers everywhere) it is much more inconvenient to iterate over pages, 
-    # but that is what is done here.
-    while next_page_token is not None:
-        # A page of data i.e. a list of videos and all needed data
-        video_data_page = api_request(next_page_token, country_code)
-
-        # Get the next page token and build a string which can be injected into the request with it, unless it's None,
-        # then let the whole thing be None so that the loop ends after this cycle
-        next_page_token = video_data_page.get("nextPageToken", None)
-        next_page_token = f"&pageToken={next_page_token}&" if next_page_token is not None else next_page_token
-
-        # Get all of the items as a list and let get_videos return the needed features
-        items = video_data_page.get('items', [])
-        country_data += get_videos(items)
-
-    return country_data
-
-
-def write_to_file(country_code, country_data):
-
-    print(f"Writing {country_code} data to file...")
-
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
-
-    with open(f"{output_dir}/{time.strftime('%y.%d.%m.%H.%M')}_{country_code}_videos.csv", "w+", encoding='utf-8') as file:
-        for row in country_data:
+    with open(f"{output_dir}/{time.strftime('%y.%m.%d.%H:%M')}_US_videos.csv", "w+", encoding='utf-8') as file:
+        for row in lines:
             file.write(f"{row}\n")
-
-
-def get_data():
-    for country_code in country_codes:
-        country_data = [",".join(header)] + get_pages(country_code)
-        write_to_file(country_code, country_data)
-
+    with open(f"{output_dir}/videos.csv", "a+", encoding='utf-8') as file:
+        for row in lines:
+            csv.writer(file).writerow(row)
+           
 
 if __name__ == "__main__":
+#     parser = argparse.ArgumentParser()
+#     parser.add_argument('--key_path', help='Path to the file containing the api key, by default will use api_key.txt in the same directory', default='api_key.txt')
+#     parser.add_argument('--country_code_path', help='Path to the file containing the list of country codes to scrape, by default will use country_codes.txt in the same directory', default='country_codes.txt')
+#     parser.add_argument('--output_dir', help='Path to save the outputted files in', default='data/')
+#     args = parser.parse_args()
+    while True:
+        output_dir = 'data/'
+        with open('api_key.txt', 'r') as file:
+            api_key = file.read().rstrip()
+        for cat in cats:
+            lines = parse_videos(api_request(by_cat=True, trending=True, catId=cat))
+            write_to_file(lines)
+            
+        get_data()
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--key_path', help='Path to the file containing the api key, by default will use api_key.txt in the same directory', default='api_key.txt')
-    parser.add_argument('--country_code_path', help='Path to the file containing the list of country codes to scrape, by default will use country_codes.txt in the same directory', default='country_codes.txt')
-    parser.add_argument('--output_dir', help='Path to save the outputted files in', default='data/')
-
-    args = parser.parse_args()
-
-    output_dir = args.output_dir
-    api_key, country_codes = setup(args.key_path, args.country_code_path)
-
-    get_data()
+        os.sleep(1800)
